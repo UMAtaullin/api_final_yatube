dbc1f8f7638b508606f2c7a597c0448445486083
Ural Ataullin <ataullinural@gmail.com>
2023-03-13 14:08:45 +0500

    permissions_review

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -21,7 +21,7 @@ class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     pagination_class = LimitOffsetPagination
-    permission_classes = [OwnerOrReadOnly]
+    permission_classes = [IsAuthenticatedOrReadOnly, OwnerOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -29,7 +29,7 @@ class PostViewSet(viewsets.ModelViewSet):
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    permission_classes = [OwnerOrReadOnly]
+    permission_classes = [IsAuthenticatedOrReadOnly, OwnerOrReadOnly]
 
     def get_queryset(self):
         post = get_object_or_404(Post, id=self.kwargs.get('post_id'))
